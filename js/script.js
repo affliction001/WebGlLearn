cbe1288adf9fc59787be0f1089433c13e4fe3a36
Vladislav Semenov <affliction001@mail.ru>
2018-11-09 23:12:10 +0300

    Добавил библиотеку three.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+const canvas = document.getElementById('canvas');
+canvas.setAttribute('width', width);
+canvas.setAttribute('height', height);
