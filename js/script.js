634d864ac390c30a91bd2b59ba7e28a40fab62ba
Vladislav Semenov <affliction001@mail.ru>
2018-11-10 19:32:06 +0300

    Создал первую сцену

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ const height = window.innerHeight;
 const canvas = document.getElementById('canvas');
 canvas.setAttribute('width', width);
 canvas.setAttribute('height', height);
+
+const renderer = new THREE.WebGLRenderer( {canvas: canvas} );
+renderer.setClearColor(0x000000);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 5000);
+camera.position.set(0, 0, 1000);
+
+const light = new THREE.AmbientLight(0xffffff);
+scene.add(light);
+
+const geometry = new THREE.SphereGeometry(200, 12, 12);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+renderer.render(scene, camera);
